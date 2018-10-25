c51e907d726c49456e7db2ff78958d1763d39473
Yashwanth H P <yeshwaanth01@gmail.com>
2018-10-25 23:07:06 +0530

    first commit with products page

---
+++ src/node_modules/containers/layout/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Layout = ({children}) => (
+    <div className='view-container'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-3'>
+                    Sidebar
+                </div>
+                <div className='col-md-9'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Layout
