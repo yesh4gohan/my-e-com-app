f8dd089dded17ecb138e2b99bdeb4d189d82c85f
Yashwanth H P <yeshwaanth01@gmail.com>
2018-11-03 13:07:19 +0530

    need to look for merge issue

--- src/index.js
+++ src/index.js
@@ -6,18 +6,19 @@ import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import {syncHistoryWithStore} from 'react-router-redux'
-import {Router, Route, browserHistory} from 'react-router'
+import {Router, Route, browserHistory,Switch} from 'react-router'
 import {Provider} from 'react-redux'
-import Basket from "containers/basket"
+
 import reducers from 'reducers'
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
 import Phone from 'containers/phone'
 import Login from 'components/Login'
 import Register from 'components/register'
-import AppNavBar from "components/navbar"
+
 import Amplify from "aws-amplify";
 import config from "./config/config"
+
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
@@ -25,23 +26,43 @@ const store = createStore(reducers, composeWithDevTools(
 
 const history = syncHistoryWithStore(browserHistory, store)
 
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+    //   identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+    // Storage: {
+    //   region: config.s3.REGION,
+    //   bucket: config.s3.BUCKET,
+    //   identityPoolId: config.cognito.IDENTITY_POOL_ID
+    // },
+    // API: {
+    //   endpoints: [
+    //     {
+    //       name: "notes",
+    //       endpoint: config.apiGateway.URL,
+    //       region: config.apiGateway.REGION
+    //     },
+    //   ]
+    // }
+  });
+
 ReactDOM.render(
     <Provider store={store}>
-        <div>
-           
         <Router history={history}>
-        
             <Route component={Layout}>
-                <Route path='/' component={Phones} />
             
-                
-                
-
+                <Route  path="/" component={Phones} />
+              
             </Route>
-            <Route  path="/basket" component={Basket} />
             <Route path = "/phone/:id" component = {Phone}/>
+            <Route path = "/login" component = {Login}/>
+            <Route path = "/register" component = {Register}/>
         </Router>
-        </div>
     </Provider>,
     document.getElementById('root')
 );
